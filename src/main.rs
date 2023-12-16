0567ea0f9e1a1885d1a9f1bbe78921458983f5de
Dragos Carmaciu <dragman@gmail.com>
2023-12-16 15:05:05 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+mod models;
+use dotenv::dotenv;
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use reqwest::Client;
+use serde_json::Value;
+use serde_xml_rs::from_str;
+use std::env;
+
+async fn departures_to(from: &str, to: Option<&str>) -> Value {
+    dotenv().ok();
+
+    let client = Client::new();
+    let key = env::var("API_KEY").unwrap();
+    let endpoint_url = env::var("API_URL").unwrap();
+
+    // Build the SOAP request payload
+    let mut soap_request = format!(
+        r#"
+        <soap-env:Envelope xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap-env:Header>
+          <ns0:AccessToken xmlns:ns0="http://thalesgroup.com/RTTI/2013-11-28/Token/types">
+            <ns0:TokenValue>{}</ns0:TokenValue>
+          </ns0:AccessToken>
+        </soap-env:Header>
+        <soap-env:Body>
+          <ns0:GetDepartureBoardRequest xmlns:ns0="http://thalesgroup.com/RTTI/2021-11-01/ldb/">
+            <ns0:numRows>10</ns0:numRows>
+            <ns0:crs>{}</ns0:crs>
+    "#,
+        key, from
+    );
+
+    // Optionally add filterCrs
+    if let Some(to_crs) = to {
+        let filter_crs = format!(r#"<ns0:filterCrs>{}</ns0:filterCrs>"#, to_crs);
+        soap_request.push_str(&filter_crs)
+    }
+
+    let soap_request_end: String = String::from(
+        r#"
+        </ns0:GetDepartureBoardRequest>
+        </soap-env:Body>
+        </soap-env:Envelope>
+    "#,
+    );
+
+    soap_request.push_str(&soap_request_end);
+
+    let response = client
+        .post(endpoint_url)
+        .header("Content-Type", "text/xml")
+        .body(soap_request)
+        .send()
+        .await;
+
+    let response_text = response.unwrap().text().await.unwrap();
+
+    let soap_response: models::SoapResponse =
+        from_str(&response_text).expect("Failed to deserialise soap response");
+
+    serde_json::to_value(&soap_response.body).unwrap()
+}
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    if let Some(query_map) = event.query_string_parameters_ref() {
+        let from_crs = query_map.first("from");
+
+        if from_crs.is_none() {
+            return Ok(Response::builder()
+                .status(400)
+                .body("No from query provided, expected from".into())
+                .expect("failed to render response"));
+        }
+
+        let to_crs = query_map.first("to");
+
+        let departures_response = departures_to(from_crs.unwrap(), to_crs).await;
+
+        let resp = Response::builder()
+            .status(200)
+            .header("content-type", "text/json")
+            .body(departures_response.to_string().into())
+            .map_err(Box::new)?;
+        return Ok(resp);
+    } else {
+        return Ok(Response::builder()
+            .status(400)
+            .body("No queries provided, expected from and to".into())
+            .expect("failed to render response"));
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
---
+++ src/models.rs
@@ -0,0 +1,82 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Location {
+    #[serde(rename = "locationName")]
+    location_name: String,
+    crs: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Destination {
+    location: Location,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Origin {
+    location: Location,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Service {
+    std: String,
+    etd: String,
+    platform: Option<i32>,
+    operator: String,
+    #[serde(rename = "operatorCode")]
+    operator_code: String,
+    length: Option<i32>,
+    #[serde(rename = "serviceID")]
+    service_id: String,
+    origin: Origin,
+    destination: Destination,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TrainServices {
+    #[serde(rename = "service")]
+    services: Vec<Service>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct StationBoard {
+    #[serde(rename = "generatedAt")]
+    generated_at: String,
+    #[serde(rename = "locationName")]
+    location_name: String,
+    crs: String,
+    #[serde(rename = "trainServices")]
+    train_services: TrainServices,
+}
+
+// #[derive(Debug, Deserialize, Serialize)]
+// struct StationBoardResult {
+//     #[serde(rename = "GetStationBoardResult")]
+//     station_board: StationBoard,
+// }
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Fault {
+    faultcode: String,
+    faultstring: String,
+    detail: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Body {
+    GetDepartureBoardResponse {
+        #[serde(rename = "GetStationBoardResult")]
+        station_board: StationBoard,
+    },
+    Fault {
+        faultcode: String,
+        faultstring: String,
+        detail: String,
+    },
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct SoapResponse {
+    #[serde(rename = "Body")]
+    pub body: Body,
+}
